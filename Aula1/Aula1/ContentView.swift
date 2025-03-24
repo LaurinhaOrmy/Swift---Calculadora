03fc6b9e0634d3425e179fdc74142874f71554a6
Laurinha! <159191021+LaurinhaOrmy@users.noreply.github.com>
2025-03-24 11:49:55 -0300

    Código atualizado

--- Aula1/Aula1/ContentView.swift
+++ Aula1/Aula1/ContentView.swift
@@ -8,32 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var idade = ""
+    @State private var IdadeHumana: String = ""
+    @State private var IdadeCanina: String = ""
+    @State private var erro: Bool = false
     
     var body: some View {
-        VStack {
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.purple]),
+                           startPoint: .topLeading, endPoint: .bottomTrailing)
             
-            Text("Calculadora de Idade Canina!")
-                .font(.largeTitle)
-                .fontWeight(.medium)
-                .foregroundColor(Color.black)
-                .padding(.top)
-            
-            Image("Dog")
-              .resizable()
-              .frame(width: 300, height: 330, alignment: .center)
-            
-            TextField(
-                    "Insira a sua idade humana:",
-                    text: $idade
-                )
+            VStack(spacing: 20) {
+                Text("Calculadora de Idade Canina!")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.black)
+                    .padding(.top)
+                
+                Image("Dog")
+                    .resizable()
+                    .frame(width: 300, height: 330, alignment: .center)
+                    .scaledToFit()
+                
+                
+                TextField("Digite a idade humana", text: $IdadeHumana)
+                    .keyboardType(.numberPad)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .frame(width: 250)
+                Button(action: calcularIdade){
+                    Text("Calcular")
+                        .padding()
+                        .frame(width: 150)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                if !IdadeCanina.isEmpty{
+                    Text("Idade em anos caninos: \(IdadeCanina)")
+                        .font(.headline)
+                        .padding()
+                }
+            }
         }
-        .padding()
+    }
+    
+    func calcularIdade(){
+        if let idade = Int(IdadeHumana), idade > 0{
+            let resultado = idade * 7
+            IdadeCanina = "\(resultado) anos"
+            erro = false
+        } else {
+            IdadeCanina = "Digite um número válido!"
+            erro = true
+        }
+        print(IdadeCanina)
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
